03709a08b9afecde8be359a742134f973b66fec4
Alex Arraiza <alexarraiza@gmail.com>
2020-11-21 19:40:47 +0100

    - Add firebase with crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:async';
+
 import 'package:daily_log/ui/app/daily_log.app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(DailyLogApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  if (kDebugMode)
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  runZonedGuarded<Future<void>>(() async {
+    runApp(DailyLogApp());
+  }, FirebaseCrashlytics.instance.recordError);
 }
